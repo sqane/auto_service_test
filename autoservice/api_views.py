72474a20bb514e17fc7dbe8011e5dbf34b2169df
Ильдар Абдулабаев <gamedozfeed@gmail.com>
2019-12-03 21:12:32 +0600

    qwe

--- autoservice/api_views.py
+++ autoservice/api_views.py
@@ -40,8 +40,8 @@ class GetUserData(APIView):
     permission_classes = (IsAuthenticated,)
     def post(self,request):
         user = request.user
-        user1 = models.User.objects.get(pk=user.pk).values('id','username','email','first_name','last_name','last_login')
-        as_user = models.AS_user.objects.get(user=user).values('id','lang__name')
+        user1 = models.User.objects.get(pk=user.pk)
+        as_user = models.AS_user.objects.get(user=user)
 
 
         return Response(user1,as_user)
